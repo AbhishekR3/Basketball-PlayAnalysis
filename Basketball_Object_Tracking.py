2b7e567e77899a0dd13cdb5800985b20e8852113
Abhishek Ramesh <abhishekramesh@gmail.com>
2024-05-14 22:26:10 -0700

    Applied mask to remove static portion of the video. Updated roadmap.

--- Basketball_Object_Tracking.py
+++ Basketball_Object_Tracking.py
@@ -9,11 +9,40 @@
 import cv2
 import numpy as np
 import os
-from ultralytics import YOLO
+import time
 
 #%%
 
-def circle_detection(p1, p2, results, frame_color):
+def preprocess_frame(frame, greyed = True, blur = 'median'):
+    """
+    Objective:
+    Preprocess frame of the video based on input parameters
+
+    Parameters:
+    [array] frame - video frame
+    [bool] greyed (Default: True) - apply a grey filter on the image
+    [string] blur (Default: 'median') - apply either median/gaussian blur
+
+    Returns:
+    [array] frame - video frame
+    """
+
+    # Convert the image to grey scale for better OpenCV processing
+    if greyed == True:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Apply blur to reduce noise
+    if blur == 'median':
+        frame = cv2.medianBlur(frame, 5) 
+    elif blur == 'gaussian':
+        frame = cv2.GaussianBlur(frame, (5,5), 1.5)
+
+    return frame
+
+
+#%%
+
+def circle_detection(p1, p2, results, frame_with_color):
     """
     Objective:
     Performs object detection using HoughCircles for each frame of the video
@@ -23,18 +52,16 @@ def circle_detection(p1, p2, results, frame_color):
     [int]   p1 - param1 for HoughCircles()
     [int]   p2 - param2 for HoughCircles()
     [dict]  results - number of circles counted for the given param1, param2 values
-    [array] frame_color - video frame
+    [array] frame_with_color - video frame
 
     Returns:
     [dict]  results - number of circles counted for the given param1, param2 values
-    [array] frame_color - video frame
-
+    [array] frame_with_color - video frame
     """
 
-    frame_greyed = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY) # Convert the image to grey scale for better OpenCV processing
-    frame_greyed = cv2.medianBlur(frame_greyed, 5) # Apply median blur to reduce noise
+    frame_greyed = preprocess_frame(frame_with_color)
 
-    frame_hsv = cv2.cvtColor(frame_color, cv2.COLOR_BGR2HSV)
+    frame_hsv = cv2.cvtColor(frame_with_color, cv2.COLOR_BGR2HSV)
     
     circles_detected = cv2.HoughCircles(frame_greyed, cv2.HOUGH_GRADIENT, 1, minDist = 3, param1 = p1, param2 = p2, minRadius = 10, maxRadius = 20)
 
@@ -45,7 +72,9 @@ def circle_detection(p1, p2, results, frame_color):
     # If circles were detected, create border and a center dot around the detected circle 
     else:
         circles_detected = np.uint16(np.around(circles_detected))
+        circle_count = len(circles_detected[0])
 
+        #"""
         # Display for each detected circles
         for ith_circle in circles_detected[0, :]:
 
@@ -58,17 +87,16 @@ def circle_detection(p1, p2, results, frame_color):
             detection_color = color_detection(center_color_hue) # Set border color
 
             # Outer Circle
-            cv2.circle(frame_color, (x_coordinate, y_coordinate), radius, detection_color, 2)
+            cv2.circle(frame_with_color, (x_coordinate, y_coordinate), radius, detection_color, 2)
 
             # Center of circle
-            cv2.circle(frame_color, (x_coordinate, y_coordinate), 2, detection_color, 3)
-        
-        circle_count = len(circles_detected[0])
+            cv2.circle(frame_with_color, (x_coordinate, y_coordinate), 2, detection_color, 3)
+        #"""
     
     # Add the number of circles detected to the results
     results[(p1, p2)] += circle_count
 
-    return results, frame_color
+    return results, frame_with_color
 
 
 #%%
@@ -76,7 +104,8 @@ def circle_detection(p1, p2, results, frame_color):
 def color_detection(color_hue):
     """
     Objective:
-    Returns the BGR value for a given hue color
+    Returns the BGR value for a given hue color.
+    Switch Red and Blue values to create a distnct circle around the players.
 
     Parameters:
     [int] color_hue - Hue value
@@ -85,13 +114,15 @@ def color_detection(color_hue):
     [tuple] color_detected - Color detected is represented in (x, x, x) format
     """
 
-
+    # Red detected -> Return BLue
     if color_hue < 10 or color_hue > 170:
         color_detected = (255, 0, 0)
 
+    # Orange
     elif 10 <= color_hue <= 30:
         color_detected = (255, 165, 0)
     
+    # Blue
     elif 100 <= color_hue <= 140:
         color_detected = (0, 0, 255)
 
@@ -101,18 +132,28 @@ def color_detection(color_hue):
 
     return color_detected
 
-
 #%%
-# Path to the video file
+""" Initialize Simulation Variables """
+
+# Path to the video file / basketball court diagram
 script_directory = os.getcwd()
 video_path = os.path.join(script_directory, 'simulation.mp4')
+basketball_court_diagram = os.path.join(script_directory, 'NBA Court Diagram.jpg')
 
 # Open the video file
 cap = cv2.VideoCapture(video_path)
 
+# Create a mask to remove basketball court diagram from the video
+mask = cv2.imread(basketball_court_diagram, cv2.IMREAD_GRAYSCALE)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+mask = cv2.resize(mask, (width, height))
+_, mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
+mask = mask.astype(np.uint8)
+
 # Parameter values to test
-param1_values = [12]
-param2_values = [15]
+param1_values = [12] # 12/13 - Best results
+param2_values = [15] # 15 - Best results
 
 # Initialize results dictionary
 resulting_values = {}
@@ -128,6 +169,17 @@ if not cap.isOpened():
     print("Error: Couldn't open the video file.")
     exit()
 
+# Create a VideoWriter object to save the output video
+output_path = os.path.join(script_directory, 'output_video.mp4')
+fourcc = cv2.VideoWriter_fourcc(*'X264') # Using x264
+FPS = cap.get(cv2.CAP_PROP_FPS)
+out = cv2.VideoWriter(output_path, fourcc, FPS, (width, height))
+
+#%%
+
+start_time = time.time()
+
+""" Main Simulation - Object Tracking """
 try: 
     # Loop through each frame in the video
     while cap.isOpened():
@@ -137,30 +189,45 @@ try:
         # If frame is read correctly ret is True
         if not ret:
             break
-        
+
+        # Inpaint the frame using the mask
+        inpainted_frame = cv2.inpaint(frame_colored, mask, 1, cv2.INPAINT_TELEA)
+            
+        #'''
         # Test different param values in the for loop
         for param1 in param1_values:
             for param2 in param2_values:
-                resulting_values, frame_colored  = circle_detection(param1, param2, resulting_values, frame_colored) # Perform circle detection
+                resulting_values, inpainted_frame  = circle_detection(param1, param2, resulting_values, inpainted_frame) # Perform circle detection
+        #'''
 
         # Display the frame
-        cv2.imshow('Basketball Object Tracking', frame_colored)
+        cv2.imshow('Basketball Object Tracking', inpainted_frame)
+
+        out.write(inpainted_frame)
 
         n_frames += 1 # Increase frame count
 
         # Press 'q' to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
+            
+        # Process only part of the file
+        if n_frames > 100:
+            break
     
-    
+
     # Print summary of results
+    print("Total time taken:", time.time()-start_time, "seconds")
+
     print ("Total number of circles that should have been detected", n_frames*11)
 
     for (param1, param2), count in resulting_values.items():
-        print(f"param1={param1}, param2={param2} -> {count} circles detected")
+        print(f"param1={param1}, param2={param2} -> {count} circles detected. {count/(n_frames*11)*100:.2f}%")
 
 
-except:
+except Exception as e:
+    print(f"An error occurred: {e}")
+    print("Total time taken:", time.time()-start_time, "seconds")
     cap.release()
     cv2.destroyAllWindows()
 
@@ -168,6 +235,3 @@ except:
 # Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
-
-
-#%%
