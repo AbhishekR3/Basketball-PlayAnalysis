f337f191b8fb4ea322b17b270088b591fb5422e4
Abhishek Ramesh <abhishekramesh@gmail.com>
2024-05-21 20:01:05 -0700

    Fixed response time out

--- update_badge_overall_test.py
+++ update_badge_overall_test.py
@@ -8,7 +8,6 @@ repo = os.getenv('GITHUB_REPO')
 gist_id = os.getenv('GIST_ID')
 token = os.getenv('GITHUB_TOKEN')
 
-
 # Workflows to check by their names
 workflows = ["Run Basketball Object Tracking", "Run Game Simulation"]  # Ensure these match the 'name' fields in your workflow files
 
@@ -21,7 +20,7 @@ headers = {
 }
 
 def get_workflow_id(workflow_name):
-    response = requests.get(api_url, headers=headers)
+    response = requests.get(api_url, headers=headers, timeout=30)
     if response.status_code == 200:
         data = response.json()
         for workflow in data['workflows']:
@@ -34,7 +33,7 @@ def get_workflow_id(workflow_name):
 
 def get_workflow_status(workflow_id):
     workflow_runs_url = f"{api_url}/{workflow_id}/runs?per_page=1"
-    response = requests.get(workflow_runs_url, headers=headers)
+    response = requests.get(workflow_runs_url, headers=headers, timeout=30)
     if response.status_code == 200:
         data = response.json()
         if data["workflow_runs"]:
@@ -72,6 +71,6 @@ headers_gist = {
     "Authorization": f"Bearer {token}",
     "Accept": "application/vnd.github.v3+json"
 }
-response = requests.patch(gist_url, headers=headers_gist, json={"files": {"badge.json": {"content": json.dumps(badge_json)}}})
+response = requests.patch(gist_url, headers=headers_gist, json={"files": {"badge.json": {"content": json.dumps(badge_json)}}}, timeout=30)
 
 print(f"Badge update response: {response.status_code}, {response.text}")
