f5ca0c8915c448308374643658c6e8e849e261c3
Abhishek Ramesh <abhishekramesh@gmail.com>
2024-05-22 18:47:35 -0700

    Improved overall structure and code quality

--- Basketball_Object_Tracking.py
+++ Basketball_Object_Tracking.py
@@ -27,18 +27,21 @@ def preprocess_frame(frame, greyed = True, blur = 'median'):
     Returns:
     [array] frame - video frame
     """
+    try:
+        # Convert the image to grey scale for better OpenCV processing
+        if greyed is True:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Convert the image to grey scale for better OpenCV processing
-    if greyed is True:
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Apply blur to reduce noise
+        if blur == 'median':
+            frame = cv2.medianBlur(frame, 5)
+        elif blur == 'gaussian':
+            frame = cv2.GaussianBlur(frame, (5,5), 1.5)
 
-    # Apply blur to reduce noise
-    if blur == 'median':
-        frame = cv2.medianBlur(frame, 5)
-    elif blur == 'gaussian':
-        frame = cv2.GaussianBlur(frame, (5,5), 1.5)
+        return frame
 
-    return frame
+    except Exception as e:
+        logger.error("Error in preprocessing the frame: %s", e)
 
 
 #%%
@@ -60,44 +63,48 @@ def circle_detection(p1, p2, results, frame_with_color):
     [array] frame_with_color - video frame
     """
 
-    frame_greyed = preprocess_frame(frame_with_color)
+    try:
+        frame_greyed = preprocess_frame(frame_with_color)
 
-    frame_hsv = cv2.cvtColor(frame_with_color, cv2.COLOR_BGR2HSV)
+        frame_hsv = cv2.cvtColor(frame_with_color, cv2.COLOR_BGR2HSV)
 
-    circles_detected = cv2.HoughCircles(frame_greyed, cv2.HOUGH_GRADIENT, 1, minDist = 3, param1 = p1, param2 = p2, minRadius = 10, maxRadius = 20)
+        circles_detected = cv2.HoughCircles(frame_greyed, cv2.HOUGH_GRADIENT, 1, minDist = 3, param1 = p1, param2 = p2, minRadius = 10, maxRadius = 20)
 
-    # If no circles were detected, set circle_count to 0
-    if circles_detected is None:
-        circle_count = 0
+        # If no circles were detected, set circle_count to 0
+        if circles_detected is None:
+            circle_count = 0
 
-    # If circles were detected, create border and a center dot around the detected circle
-    else:
-        circles_detected = np.uint16(np.around(circles_detected))
-        circle_count = len(circles_detected[0])
+        # If circles were detected, create border and a center dot around the detected circle
+        else:
+            circles_detected = np.uint16(np.around(circles_detected))
+            circle_count = len(circles_detected[0])
 
-        #"""
-        # Display for each detected circles
-        for ith_circle in circles_detected[0, :]:
+            #"""
+            # Display for each detected circles
+            for ith_circle in circles_detected[0, :]:
 
-            x_coordinate = ith_circle[0]
-            y_coordinate = ith_circle[1]
-            radius = ith_circle[2]
+                x_coordinate = ith_circle[0]
+                y_coordinate = ith_circle[1]
+                radius = ith_circle[2]
 
-            # Color of circle
-            center_color_hue = frame_hsv[y_coordinate, x_coordinate][0] # Set color hue of the circle
-            detection_color = color_detection(center_color_hue) # Set border color
+                # Color of circle
+                center_color_hue = frame_hsv[y_coordinate, x_coordinate][0] # Set color hue of the circle
+                detection_color = color_detection(center_color_hue) # Set border color
 
-            # Outer Circle
-            cv2.circle(frame_with_color, (x_coordinate, y_coordinate), radius, detection_color, 2)
+                # Outer Circle
+                cv2.circle(frame_with_color, (x_coordinate, y_coordinate), radius, detection_color, 2)
 
-            # Center of circle
-            cv2.circle(frame_with_color, (x_coordinate, y_coordinate), 2, detection_color, 3)
-        #"""
+                # Center of circle
+                cv2.circle(frame_with_color, (x_coordinate, y_coordinate), 2, detection_color, 3)
+            #"""
 
-    # Add the number of circles detected to the results
-    results[(p1, p2)] += circle_count
+        # Add the number of circles detected to the results
+        results[(p1, p2)] += circle_count
 
-    return results, frame_with_color
+        return results, frame_with_color
+    
+    except Exception as e:
+        logger.error("Error in circle detection: %s", e)
 
 
 #%%
@@ -115,51 +122,58 @@ def color_detection(color_hue):
     [tuple] color_detected - Color detected is represented in (x, x, x) format
     """
 
-    # Red detected -> Return BLue
-    if color_hue < 10 or color_hue > 170:
-        color_detected = (255, 0, 0)
+    try:
+        # Red detected -> Return BLue
+        if color_hue < 10 or color_hue > 170:
+            color_detected = (255, 0, 0)
 
-    # Orange
-    elif 10 <= color_hue <= 30:
-        color_detected = (255, 165, 0)
+        # Orange
+        elif 10 <= color_hue <= 30:
+            color_detected = (255, 165, 0)
 
-    # Blue
-    elif 100 <= color_hue <= 140:
-        color_detected = (0, 0, 255)
+        # Blue
+        elif 100 <= color_hue <= 140:
+            color_detected = (0, 0, 255)
 
-    else:
-        logger.debug("Color hue outside of range: %s", color_hue)
-        color_detected = (0,0,0)
+        else:
+            logger.debug("Color hue outside of range: %s", color_hue)
+            color_detected = (0,0,0)
 
-    return color_detected
+        return color_detected
 
+    except Exception as e:
+        logger.error("Error in detecting color: %s", e)
 
 #%% Configuring logging
 
-log_file_path = 'object_tracking_output.log'
+try:
+    log_file_path = 'object_tracking_output.log'
 
-# Check if the file exists
-if os.path.exists(log_file_path):
-    # Delete the file
-    os.remove(log_file_path)
-    print(f"The file {log_file_path} has been deleted.")
-else:
-    print(f"No file found with the name {log_file_path}.")
+    # Check if the file exists
+    if os.path.exists(log_file_path):
+        # Delete the file
+        os.remove(log_file_path)
+        print(f"The file {log_file_path} has been deleted.")
+    else:
+        print(f"No file found with the name {log_file_path}.")
 
-# Create a logger object
-logger = logging.getLogger('ObjectTrackingLogger')
-logger.setLevel(logging.DEBUG)  # Set the minimum log level to debug
+    # Create a logger object
+    logger = logging.getLogger('ObjectTrackingLogger')
+    logger.setLevel(logging.DEBUG)  # Set the minimum log level to debug
 
-# Create file handler which logs even debug messages
-file_handler = logging.FileHandler(log_file_path)
-file_handler.setLevel(logging.DEBUG)
+    # Create file handler which logs even debug messages
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
 
-# Create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
+    # Create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
 
-# Add the handlers to the logger
-logger.addHandler(file_handler)
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+
+except Exception as e:
+    logger.error("Error in creating logging configuration: %s", e)
 
 
 #%% Initialize Simulation Variables
@@ -173,12 +187,17 @@ basketball_court_diagram = os.path.join(script_directory, 'assets/Basketball Cou
 cap = cv2.VideoCapture(video_path)
 
 # Create a mask to remove basketball court diagram from the video
-mask = cv2.imread(basketball_court_diagram, cv2.IMREAD_GRAYSCALE)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-mask = cv2.resize(mask, (width, height))
-_, mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
-mask = mask.astype(np.uint8)
+try:
+    mask = cv2.imread(basketball_court_diagram, cv2.IMREAD_GRAYSCALE)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    mask = cv2.resize(mask, (width, height))
+    _, mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
+    mask = mask.astype(np.uint8)
+
+except:
+    logger.error ("Error: Couldn't open the basketball court diagram file.")
+    exit()
 
 # Parameter values to test
 param1_values = [12] # 12/13 - Best results
@@ -199,7 +218,7 @@ if not cap.isOpened():
     exit()
 
 # Create a VideoWriter object to save the output video
-output_path = os.path.join(script_directory, 'assets/output_video.mp4')
+output_path = os.path.join(script_directory, 'assets/object_tracking_video.mp4')
 fourcc = cv2.VideoWriter_fourcc(*'avc1') # Using avc1
 FPS = cap.get(cv2.CAP_PROP_FPS)
 out = cv2.VideoWriter(output_path, fourcc, FPS, (width, height))
@@ -235,21 +254,21 @@ try:
 
         # Press 'q' to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
+            logger.debug ("Simulation stopped through manual intervention")
             break
 
         # For git actions testing, stop simulation to focus on testing code
         if n_frames > 0 and os.getenv('GITHUB_ACTIONS') is True:
-            logger.debug("Simulation stopped, due to being tested in github actions")
+            logger.debug ("Simulation stopped, due to being tested in github actions")
             break
 
     # Log results summary
-    logger.debug (f"Total number of circles that should have been detected {n_frames*11}%%")
+    logger.debug (f"Total number of circles that should have been detected {n_frames*11}")
 
     for (param1, param2), count in resulting_values.items():
-        logger.debug (f"param1={param1}, param2={param2} -> {count} circles detected. {count/(n_frames*11)*100:.2f}%")
+        logger.debug (f"param1={param1}, param2={param2} -> {count} circles detected. Detected {count/(n_frames*11)*100:.2f}%")
 
     logger.debug("Object Tracking succeeded")
-
 
 except Exception as e:
     logger.error (f"An error occurred: {e}")
